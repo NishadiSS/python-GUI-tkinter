5452f86ee6c18dceda2327651114e9db9e3d0ea6
nishadi260 <nishadisasanthika260@gmail.com>
2023-12-14 16:38:56 +0530

    How to create radio button

--- 4. Radio Button/student.py
+++ 4. Radio Button/student.py
@@ -7,15 +7,13 @@ root.iconbitmap('icon.ico')
 root.geometry('300x200')
 root.resizable(False,False)
 
-radio_var = tk.StringVar()
-
 label5 = tk.Label(root, text = "Gender",font="time 12 bold")
 label5.pack()
 
-radio1 = tk.Radiobutton(root,text='Male',value='Male',variable=radio_var,font="time 10")
+radio1 = tk.Radiobutton(root,text='Male',value='Male',font="time 10")
 radio1.pack()
 
-radio2 = tk.Radiobutton(root,text='Female',value='Female',variable=radio_var,font="time 10")
+radio2 = tk.Radiobutton(root,text='Female',value='Female',font="time 10")
 radio2.pack()
 
 root.mainloop()
